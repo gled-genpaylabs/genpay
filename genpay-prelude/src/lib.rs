8973346a3113e1c8e779746abf502a4afac4e447
Gledson Joaquim Lopes <gledsonjoaquimlopes@Gledsons-MacBook-Pro.local>
2025-08-29 01:41:33 -0300

    facelift

--- genpay-prelude/src/lib.rs
+++
@@ -1,7 +0,0 @@
-use once_cell::sync::Lazy;
-use std::sync::Mutex;
-use genpay_lexer::token::Token;
-
-/// Static, globally accessible vector for lexer tokens.
-/// All crates should use this to avoid re-lexing or reading the file twice.
-pub static TOKENS: Lazy<Mutex<Vec<Token<'static>>>> = Lazy::new(|| Mutex::new(Vec::new()));
